f4a11ba8e4e2d509e72d3c9ac1ac754180d361d6
Richard Mayes <richard.mayes@watchfinder.co.uk>
2021-12-01 17:35:13 +0000

    refactor(day-one): use map to parse the file straight into a vector

--- day-one/part-one/src/main.rs
+++ day-one/part-one/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("../input.txt");
+    let contents = fs::read_to_string("./input.txt");
     let contents = match contents {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
@@ -9,28 +9,22 @@ fn main() {
 
     let lines = contents.split('\n');
 
+    let numbers: Vec<i32> = lines
+        .enumerate()
+        .map(|(_, value)| value.parse::<i32>().unwrap_or_default())
+        .collect();
+
     let mut answer = 0;
     let mut previous_value = 0;
-    for (idx, line) in lines.enumerate() {
-        if line.is_empty() {
-            continue;
-        }
 
-        let num = line.parse::<i32>();
-        let num = match num {
-            Ok(num) => num,
-            Err(error) => {
-                println!("Error parsing number '{}' - {:?}, skipping...", line, error);
-                continue;
-            }
-        };
-
-        if idx > 0 && previous_value < num {
+    for (idx, num) in numbers.iter().enumerate() {
+        if idx > 0 && previous_value < *num {
             println!("{} is less than {}", previous_value, num);
             answer += 1;
         }
 
-        previous_value = num;
+        previous_value = *num;
     }
+
     println!("The answer is {}", answer);
 }
